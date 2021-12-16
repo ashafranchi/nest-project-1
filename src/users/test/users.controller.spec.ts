32c2e45895b2189af622a3e75c4289c6312834af
Flyreel 074 <flyreel074@Asha-Franchi-074-MacBook-Pro.local>
2021-12-16 14:43:35 -0800

    halfway through unit testing tutorial

--- src/users/test/users.controller.spec.ts
+++ src/users/test/users.controller.spec.ts
@@ -1,5 +1,21 @@
-// describe('UsersController', () => {
-//     beforeEach(async() => {
+import { Test } from "@nestjs/testing";
+import { UsersController } from "../users.controller"
+import { UsersService } from "../users.service";
 
-//     })
-// })
+jest.mock('../../users.service');
+
+describe('UsersController', () => {
+    let usersController: UsersController;
+    let usersService: UsersService;
+    beforeEach(async() => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [],
+            controllers: [UsersController],
+            providers: [UsersService]
+        }).compile();
+
+        usersController = moduleRef.get<UsersController>(UsersController);
+        usersService = moduleRef.get<UsersService>(UsersService);
+        jest.clearAllMocks();
+    })
+})
