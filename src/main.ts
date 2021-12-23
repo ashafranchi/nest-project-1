d4962ff8f795d0fbe37965075a637daa776545b1
Flyreel 074 <flyreel074@Asha-Franchi-074-MacBook-Pro.local>
2021-12-23 10:03:23 -0800

    auth initialized

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     .setDescription('Groomer and Pet API')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api', app, document);
 
